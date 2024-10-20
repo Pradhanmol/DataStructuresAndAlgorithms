6e5fc4668a7f028a947a5a9c4ed3b339f9774233
anmolPradhan <admin@Anmol-MacBook-Air-2.local>
2024-10-20 11:39:05 +0530

    add few files related to linkedlist & tree

---
+++ tree/apnacollegetree/BinaryTree.java
@@ -0,0 +1,17 @@
+package tree.apnacollegetree;
+
+public class BinaryTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void main(String[] args){
+        int node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // pre-order sequence
+    }
+}
